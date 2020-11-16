79a29ca575e966a141f3e1218d9a94deca871f8b
agrembo <42969522+agrembo@users.noreply.github.com>
2020-11-16 15:37:05 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-states-demo"
-    key            = "terraform.tfstate"
+    key            = "vault-dev-terraform.tfstate"
     region         = "us-east-1"
   }
 }
